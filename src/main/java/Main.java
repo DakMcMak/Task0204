89886d9af854a862360607d855902f99ded60e73
DakMcMak <esbolov.galymzhan@mail.ru>
2017-03-14 21:52:28 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Man man = new  Man();
+        Woman woman = new Woman();
+        man.wife = new Woman();
+        woman.husband = new Man();
+    }
+
+    public static class Man {
+        public int age;
+        public int height;
+        public Woman wife;
+    }
+
+    public static class Woman {
+        public int age;
+        public int height;
+        public Man husband;
+    }
+}
+
